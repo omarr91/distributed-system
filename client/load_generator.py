d2e20e43c55beb700993f356b9c15eea9e42f698
Omar <omarashraf91995@gmail.com>
2026-04-26 17:17:38 +0300

    ely hy'rab mno hytrd

--- client/load_generator.py
+++ client/load_generator.py
@@ -1,29 +1,22 @@
+# client/load_generator.py
+
 import threading
 from common.models import Request
-from monitoring.metrics import Metrics
 
-def simulate_user(scheduler, user_id, metrics):
+def simulate_user(scheduler, user_id):
     request = Request(id=user_id, query=f"Query {user_id}")
     response = scheduler.handle_request(request)
-
-    metrics.record(response["latency"])
 
     print(f"[Client] Response {response['id']} | Latency: {response['latency']:.3f}s")
 
 
 def run_load_test(scheduler, num_users=1000):
     threads = []
-    metrics = Metrics()
-
-    metrics.start()
 
     for i in range(num_users):
-        t = threading.Thread(target=simulate_user, args=(scheduler, i, metrics))
+        t = threading.Thread(target=simulate_user, args=(scheduler, i))
         threads.append(t)
         t.start()
 
     for t in threads:
         t.join()
-
-    metrics.end()
-    metrics.report()
