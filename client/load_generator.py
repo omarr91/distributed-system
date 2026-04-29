8e470fd075fbd3e0272132dbabf012e9e8ca9c65
Omar <omarashraf91995@gmail.com>
2026-04-29 18:04:14 +0300

    removed test folder

--- client/load_generator.py
+++ client/load_generator.py
@@ -1,6 +1,8 @@
 # client/load_generator.py
 
 import threading
+import time
+from queue import Queue
 from common.models import Request
 
 def simulate_user(scheduler, user_id, result_list=None):
@@ -10,3 +12,34 @@ def simulate_user(scheduler, user_id, result_list=None):
         result_list.put(response)
         return
     return response
+
+def run_load_test(scheduler, num_users=1000):
+    threads = []
+    collected = Queue()
+
+    start_time = time.time()
+
+    for i in range(num_users):
+        t = threading.Thread(target=simulate_user, args=(scheduler, i,collected))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    end_time = time.time()
+    results = []
+    while not collected.empty():
+        results.append(collected.get())
+
+    throughput = len(results) / (end_time - start_time)
+
+    workers_ids = set()
+    for i in range(len(results)):
+        workers_ids.add(results[i]["worker_id"])
+    
+    num_of_workers = len(workers_ids)
+    avg_latency = 0
+    for i in range(len(results)): avg_latency += results[i]["latency"]
+
+    avg_latency = avg_latency / len(results)
